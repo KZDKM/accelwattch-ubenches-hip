b6b74231a9908cd4347fc89cdd0903ab63df217f
Kevin Zhu <kzdkm@login1.hpcfund>
2026-07-10 08:21:52 -0500

    memory: fixed hipify

--- launch_scripts/run-accelwattch-ubenches.sh
+++ launch_scripts/run-accelwattch-ubenches.sh
@@ -1,15 +1,16 @@
 # Change below to be where you want results kept
-RESULTS_DIR="/home1/wmadsmith/accelwattch_ubench_results"
+RESULTS_DIR="/home1/kzdkm/accelwattch_ubench_results"
 # Change below to be where the parent directory of your accelwattch ubench
 # repo (i.e., functional_benchmarks/ and such should be under here)
-BIN_DIR="../"
-ITERATIONS=1000000
+BIN_DIR="/home1/kzdkm/accelwattch-ubenches-hip"
+ITERATIONS=10000000
+CORES=608
 
 # Location of rocprofwrap-lt tool.
-ROCPROFWRAP_DIR=/home1/wmadsmith/rocprofwrap
+ROCPROFWRAP_DIR=/home1/kzdkm/rocprofwrap
 make -C ${ROCPROFWRAP_DIR}/rocprofwrap_lt
 make -C ${BIN_DIR} -f Makefile-hip
-if [[ -d "$RESULTS_DIR" ]]; then
+if [[ ! -d "$RESULTS_DIR" ]]; then
         echo "Creating results dir! $RESULTS_DIR"
         mkdir -p $RESULTS_DIR
 fi
@@ -18,11 +19,11 @@ fi
 for exe in "$BIN_DIR"/bin/*; do
     ubench=$(basename "$exe")
     WRAPPER_CMD=(
-     python3 "${ROCPROFWRAP_DIR}/rocprofwrap_lt/wrapper.py" 
-     --devices 0 
-     --prefix="${ubench}_profiled.csv" 
+     python3 "${ROCPROFWRAP_DIR}/rocprofwrap_lt/wrapper.py"
+     --devices 0
+     --prefix="${ubench}_profiled.csv"
      --
-     "$exe" "$ITERATIONS"
+     "$exe" "$ITERATIONS" "$CORES"
     )
     # Verify the file exists and has executable permissions
     if [ -f "$exe" ] && [ -x "$exe" ]; then
@@ -34,4 +35,5 @@ for exe in "$BIN_DIR"/bin/*; do
     fi
     echo "Output to ${RESULTS_DIR}/${ubench}_profiled.csv"
 done
+
 
